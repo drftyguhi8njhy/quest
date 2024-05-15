34d6461595963f55c1ddcd8fec1d051996e43f10
drftyguhi8njhy <js1aglaquay@hotmail.com>
2024-05-16 01:55:01 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
